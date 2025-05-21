93ea0cad6f82d70e4902e347dfd7d0bd83a52e5c
Aadish Jain <aadishhere@gmail.com>
2025-05-21 09:25:25 +0530

    Initial Commit

---
+++ swipeUpCardStack/swipeUpCardStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  swipeUpCardStackApp.swift
+//  swipeUpCardStack
+//
+//  Created by Aadish Jain on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct swipeUpCardStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
